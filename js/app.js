e988fb3f15605da217c3942296e46988f754d895
Rob Robinson <robrobinsoncsuf@gmail.com>
2013-12-18 09:31:15 -0800

    Cleaned up js, and index

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
+/*
+* app.js for the toy-gnome / gestures project...
+*/
 
-var mouseIsDown = false;
-var clickX;
-var clickY;
-var releaseX;
-var releaseY;
-
+// load four images for the toy gnome's states
 var happy = new Image();
 happy.src = "./img/gnome-happy.jpg";
 
@@ -17,125 +15,137 @@ left.src = "./img/gnome-left.jpg";
 var right = new Image();
 right.src = "./img/gnome-right.jpg";
 
-var Evt = {
-	
+var start = new Image();
+start.src = "./img/gnome.jpg";
+
+
+var States = {
+	start : function(){
+		console.log("toy gnome start state...");
+		document.getElementById("gnome").src = start.src;		
+	},
+	angry : function(){
+		console.log("toy gnome is mad...");
+		document.getElementById("gnome").src = mad.src;
+	},
+	happy : function(){
+		console.log("toy gnome is happy...");
+		document.getElementById("gnome").src = happy.src;		
+	},
+	lookLeft : function(){
+		console.log("look left:");
+		document.getElementById("gnome").src = left.src;
+	},
+	lookRight : function(){
+		console.log("look right:");
+		document.getElementById("gnome").src = right.src;
+	}
 };
 
-function onMouseMove(evt) {
-    'use strict';
-    evt.preventDefault();
-}
+// Evt object will hold our event functions:
+var Evt = {
+	mouseIsDown : false,
+	clickX : null,
+	clickY : null,
+	releaseX : null,
+	releaseY : null,
 
-function onMouseStart(e) {
-    'use strict';
-    e.preventDefault();
-    //console.log('hhh');
+	onMouseMove : function(evt) {'use strict';
+		evt.preventDefault();
+	},
 
-    if (e.changedTouches && e.changedTouches.length > 0) {
-        clickX = e.changedTouches[0].pageX;
-        clickY = e.changedTouches[0].pageY;
-    } else {
-        clickX = e.pageX;
-        clickY = e.pageY;
-    }
+	onMouseStart : function(e) {'use strict';
+		e.preventDefault();
 
-    mouseIsDown = true;
-}
+		if (e.changedTouches && e.changedTouches.length > 0) {
+			this.clickX = e.changedTouches[0].pageX;
+			this.clickY = e.changedTouches[0].pageY;
+		} else {
+			this.clickX = e.pageX;
+			this.clickY = e.pageY;
+		}
 
-function onMouseEnd(e) {
-    'use strict';
-    e.preventDefault();
-    mouseIsDown = false;
+		this.mouseIsDown = true;
+	},
 
-    if (e.changedTouches && e.changedTouches.length > 0) {
-        releaseX = e.changedTouches[0].pageX;
-        releaseY = e.changedTouches[0].pageY;
-     } else {
-        releaseX = e.pageX;
-        releaseY = e.pageY; 
-    }   
-        
-    var xOry = (Math.abs(releaseX-clickX)>Math.abs(releaseY-clickY)) ? "x" : "y";
-    console.log(xOry);
-    
-    switch (xOry) {
-    	case 'x':
-    		if(releaseX-clickX > 0) {
-    			// increment
-    			console.log("look left:");
-    			document.getElementById("gnome").src = left.src;
-				//reDraw();
-	    	}
-    		else {
-    			// decrement
-    			console.log("look right:");
-    			document.getElementById("gnome").src = right.src;
-				//reDraw();
-    		}
-    	break;
-    	
-    	case 'y':
-    	
-    		if(releaseY-clickY <= 0) {
-    			
-    			console.log("toy gnome is happy...");
-    			document.getElementById("gnome").src = happy.src;
-    		} else {
-    			
-    			console.log("toy gnome is mad...");
-    			document.getElementById("gnome").src = mad.src;
-    		}
-    	break;
-    }
-    
-}	
+	onMouseEnd : function(e) {'use strict';
+		e.preventDefault();
+		this.mouseIsDown = false;
 
+		if (e.changedTouches && e.changedTouches.length > 0) {
+			this.releaseX = e.changedTouches[0].pageX;
+			this.releaseY = e.changedTouches[0].pageY;
+		} else {
+			this.releaseX = e.pageX;
+			this.releaseY = e.pageY;
+		}
 
-window.addEventListener("load",function(){
-	reDraw();
-},false);
+		var xOry = (Math.abs(this.releaseX - this.clickX) > Math.abs(this.releaseY - this.clickY)) ? "x" : "y";
+		console.log(xOry);
 
-function reDraw(callback) {
-	// var item = vocab[level];
-	// document.getElementById('content').innerHTML = item.words[subIndex];
-	// document.getElementById('level').innerHTML = "Level:" + (level+1) + " use &uarr; &rarr; &darr; &larr;";
+		switch (xOry) {
+			case 'x':
+				if (this.releaseX - this.clickX > 0) {
+					States.lookLeft();
+				} else {
+					States.lookRight();
+				}
+				break;
 
-	if(callback) {
-		callback();
+			case 'y':
+
+				if (this.releaseY - this.clickY <= 0) {
+					States.happy();
+				} else {
+					States.angry();
+				}
+				break;
+		}
+
+	},
+
+	KeyCheck : function(event) {'use strict';
+
+		var KeyID = event.keyCode;
+
+		if (KeyID === 39) {
+			// right arrow
+			States.lookRight();
+			
+		} else if (KeyID === 37) {
+			// left arrow
+			States.lookLeft();
+			
+		} else if (KeyID === 38) {
+			// up arrow
+			States.happy();
+			
+		} else if (KeyID === 40) {
+			// down arrow
+			States.angry();
+
+		} else if (KeyID === 32) {
+			// space bar
+			
+		} else {
+			// any other key
+			States.start();
+			
+		}
 	}
-}
+};
 
-function KeyCheck(event) {
-    'use strict';
-    
-    var KeyID = event.keyCode;
+// event listeners....
+window.addEventListener("load", function() {
+	States.start();
+}, false);
 
-    if (KeyID === 39) {
-        console.log('right');
-        reDraw();
-    } else if (KeyID === 37) {
-        console.log('left'); 
-        reDraw();
-    }  else if (KeyID === 38) {
-        console.log('up'); 
-        reDraw();
-    } else if (KeyID === 40) {
-        console.log('down'); 
-        reDraw();
+window.addEventListener('keydown', Evt.KeyCheck, true);
 
-    } else if(KeyID === 32) {
-    	//reDraw(playSound);
-    } else {
-    	//console.log(KeyID);
-    }
-}
-	
-	window.addEventListener('keydown',KeyCheck,true);  
-	
-	document.body.addEventListener('mousemove', onMouseMove, false);
-	document.body.addEventListener('mousedown', onMouseStart, false);
-	document.body.addEventListener('mouseup', onMouseEnd, false);
-	
-	document.body.addEventListener('touchmove', onMouseMove, false);
-	document.body.addEventListener('touchstart', onMouseStart, false);
-	document.body.addEventListener('touchend', onMouseEnd, false);	
+document.body.addEventListener('mousemove', Evt.onMouseMove, false);
+document.body.addEventListener('mousedown', Evt.onMouseStart, false);
+document.body.addEventListener('mouseup', Evt.onMouseEnd, false);
+
+document.body.addEventListener('touchmove', Evt.onMouseMove, false);
+document.body.addEventListener('touchstart', Evt.onMouseStart, false);
+document.body.addEventListener('touchend', Evt.onMouseEnd, false);
